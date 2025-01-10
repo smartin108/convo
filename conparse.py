e0dd2f9f835ba49c40eb073a3c9956ccf2328bb8
Andy Holaday <smartin108@gmail.com>
2025-01-10 14:00:13 -0500

    automatic commit

--- conparse.py
+++ conparse.py
@@ -2,39 +2,72 @@ import xmltodict
 import pprint
 import json
 
-source_file = './sms-20241206001051.xml'
 
-with open(source_file, 'r', encoding='utf-8') as f:
-    my_xml = f.read()
+def message_parts(message:str):
+    pass
 
 
-s0 = xmltodict.parse(my_xml)
-s1 = s0['smses']
+def main():
+    source_file = './sms-20241206001051.xml'
 
-for k,v in s1.items():
-    if type(v) == 'str':
-        print(f'{k}: {v}')
-    elif k == 'mms':
-        counter = 0
-        print(f'  {k}[{counter}]')
-        for vv in v:
-            print(f'    date: {vv["@date"]}')
-            print(f'    address: {vv["@address"]}')
-            print(f'    parts: {str(vv["parts"])[:100]}')
-            for part in vv["parts"].values():
-                print(f'      part: {type(part)}  {str(part)[:100]}')
-                # if part['@seq'] == '0':
-                #     print(f'      {part["@text"]}')
-            print()
-            counter += 1
-            if counter > 10:
-                break
-    elif k == 'sms':
-        print(f'  {k}')
-        for vv in v:
-            print(f'    date: {vv["@date"]}')
-            print(f'    address: {vv["@address"]}')
-            print(f'    body: {vv["@body"]}')
-            print()
-    else:
-        print(f'{k}: ({type(v)}) v: ')
+    with open(source_file, 'r', encoding='utf-8') as f:
+        my_xml = f.read()
+
+
+    s0 = xmltodict.parse(my_xml)
+    s1 = s0['smses']
+
+    for k,v in s1.items():
+        if type(v) == 'str':
+            print(f'{k}: {v}')
+        elif k == 'mms':
+            counter = 0
+            print(f'  {k}[{counter}]')
+            for vv in v:
+                print(f'    date: {vv["@date"]}')
+                # print(f'    address: {vv["@address"]}')
+                # print(f'    parts: {str(vv["parts"])[:100]}')
+                for part in vv["parts"].values():
+                    author = 'Rebecca'
+                    if isinstance(part, list):
+                        # print(f'      part: {str(part)[:100]}')
+                        for od in part:
+                            seq_flag = int(od['@seq'])
+                            if seq_flag == -1:
+                                author = 'Andy'
+                            else:
+                            # if int(od['@seq']) > -1:
+                                # print(f'      part seq={od["""@seq"""]}: {od}')
+                                print(f'      author={author}')
+                                print(f'      text={od["@text"]}')
+                    elif isinstance(part, dict):
+                        # print(f'          {part}')
+                        if part["@seq"] == '-1':
+                            author = 'Andy'
+                        print(f'      author={author}')
+                        m_text = part["@text"]
+                        data_only = False
+                        if m_text == 'null':
+                            if part["@data"]:
+                                data_only = True
+                                m_text = '<data only>'
+                        print(f'      text={m_text}')
+                    else:
+                        print(f'          unknown type {type(part)}')
+                        print(f'          content={part}')
+                print()
+                counter += 1
+                if counter > 100000:
+                    break
+        elif k == 'sms':
+            print(f'  {k}')
+            for vv in v:
+                print(f'    date: {vv["@date"]}')
+                print(f'    address: {vv["@address"]}')
+                print(f'    body: {vv["@body"]}')
+                print()
+        else:
+            print(f'{k}: ({type(v)}) v: ')
+
+if __name__ == '__main__':
+    main()
