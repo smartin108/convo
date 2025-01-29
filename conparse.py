5eadc4007c21ea3db1efda92951f82e2a4e8be4d
Andy Holaday <smartin108@gmail.com>
2025-01-29 14:00:15 -0500

    automatic commit

--- conparse.py
+++ conparse.py
@@ -6,9 +6,19 @@ from datetime import datetime
 import tzlocal
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+from os import walk
+from os import remove
+from sys import exit
 
 
 local_timezone = tzlocal.get_localzone()
+
+
+def get_file_from_backups():
+    backup_location = "H:/OneDrive/Apps/SMS Backup and Restore"
+    for subdir, _, files in walk(backup_location):
+        for filename in files:
+            yield f'{subdir}/{filename}'
 
 
 def user_get_file():
@@ -41,56 +51,58 @@ def main():
         local_time = datetime.fromtimestamp(float(unix_timestamp)/1000, local_timezone)
         all_data.append([unix_timestamp, local_time, author, text])
 
-    source_file = user_get_file()
-    all_data = []
+    # source_file = user_get_file()
+    for source_file in get_file_from_backups():
+        all_data = []
 
-    print('reading file data...')
-    with open(source_file, 'r', encoding='utf-8') as f:
-        my_xml = f.read()
+        print('reading file data...')
+        with open(source_file, 'r', encoding='utf-8') as f:
+            my_xml = f.read()
 
-    print(f'read {len(my_xml)} thingies')
-    s0 = xmltodict.parse(my_xml)
-    s1 = s0['smses']
+        print(f'read {len(my_xml)} thingies')
+        s0 = xmltodict.parse(my_xml)
+        s1 = s0['smses']
 
-    print('parsing content...')
-    for k,v in s1.items():
-        if k == 'mms':
-            counter = 0
-            for vv in v:
-                date = vv["@date"]
-                for part in vv["parts"].values():
-                    author = 'Rebecca'
-                    if isinstance(part, list):
-                        for od in part:
-                            seq_flag = int(od['@seq'])
-                            if seq_flag == -1:
+        print('parsing content...')
+        for k,v in s1.items():
+            if k == 'mms':
+                counter = 0
+                for vv in v:
+                    date = vv["@date"]
+                    for part in vv["parts"].values():
+                        author = 'Rebecca'
+                        if isinstance(part, list):
+                            for od in part:
+                                seq_flag = int(od['@seq'])
+                                if seq_flag == -1:
+                                    author = 'Andy'
+                                else:
+                                    text = od["@text"]
+                        elif isinstance(part, dict):
+                            if part["@seq"] == '-1':
                                 author = 'Andy'
-                            else:
-                                text = od["@text"]
-                    elif isinstance(part, dict):
-                        if part["@seq"] == '-1':
-                            author = 'Andy'
-                        text = part["@text"]
-                    else:
-                        author = '<unknown>'
-                        text = '<unknown>'
+                            text = part["@text"]
+                        else:
+                            author = '<unknown>'
+                            text = '<unknown>'
+                    # all_data.append([date, datetime.utcfromtimestamp(float(date)/1000).isoformat(), author, text])
+                    do_append(date, author, text)
+            elif k == 'sms':
+                for vv in v:
+                    author = 'Rebecca' # I don't actually know how to determine the author for sms message types
+                    date = vv["@date"]
+                    text = vv["@body"]
                 # all_data.append([date, datetime.utcfromtimestamp(float(date)/1000).isoformat(), author, text])
                 do_append(date, author, text)
-        elif k == 'sms':
-            for vv in v:
-                author = 'Rebecca' # I don't actually know how to determine the author for sms message types
-                date = vv["@date"]
-                text = vv["@body"]
-            # all_data.append([date, datetime.utcfromtimestamp(float(date)/1000).isoformat(), author, text])
-            do_append(date, author, text)
-        elif type(v) == 'str':
-            pass
-        else:
-            pass
-    print(f'{len(all_data)} records read\n')
-    print('writing to database...')
-    write_to_db(all_data)
-    load_db()
+            elif type(v) == 'str':
+                pass
+            else:
+                pass
+        print(f'{len(all_data)} records read\n')
+        print('writing to database...')
+        write_to_db(all_data)
+        load_db()
+        remove(source_file)
     print('done')
 
 if __name__ == '__main__':
