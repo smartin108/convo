b12971b86652e1229ad51f64b9e03447dbbb301c
Andy Holaday <smartin108@gmail.com>
2025-02-01 14:00:05 -0500

    automatic commit

---
+++ conout1.py
@@ -0,0 +1,37 @@
+import clsSQLServer
+
+S = clsSQLServer.Interface(database='Convo')
+
+q = """select converted_timestamp, author, body from Convo.dbo.conv order by converted_timestamp"""
+
+data = S.SelectQuery(q)
+
+# print(data)
+
+
+def headers(frame_width):
+    return '<html><style><table {width: %s}</style><body><table>\n'%(frame_width)
+
+
+def trailers():
+    return '</table></body></html>\n'
+
+
+def make_format(frame_width, body, align):
+    return '    <tr><td>%s</td></tr>\n'%(body)
+
+
+frame_width = '50%'
+outfile = './out.html'
+with open(outfile, 'w', encoding='UTF-8') as f:
+    f.write(headers(frame_width))
+    for r in data:
+        if r[1] == 'Rebecca':
+            align = 'left'
+        elif r[1] == 'Andy':
+            align = 'right'
+        else:
+            print(f'unknown author in record:\n{r}')
+        text = make_format(frame_width, r[2], align)
+        f.write(text)
+    f.write(trailers())
