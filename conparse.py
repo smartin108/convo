ed2c880a966eb19730b19ac03cbc1b0470ebb055
Andy Holaday <smartin108@gmail.com>
2025-03-22 13:00:27 -0400

    working on adding MIME data extraction

--- conparse.py
+++ conparse.py
@@ -27,16 +27,20 @@ def user_get_file():
     return source_file
 
 
-def write_to_db(chonk:list):
+def write_to_db(text_data:list, MIME_data:list):
     s = clsSQLServer.Interface(database='Convo')
     d = datetime.now(local_timezone)
-    for i in chonk:
+    for i in text_data:
         i.append(d)
-    # print(chonk)
-    sql = \
+    text_sql = \
 """insert into Convo.dbo.load_conv(source_timestamp, converted_timestamp, author, body, record_created) 
 values (?, ?, ?, ?, ?);"""
-    s.InsertMany(sql, chonk)
+    s.InsertMany(text_sql, text_data)
+    MIME_sql = \
+"""insert into Convo.dbo.load_multimedia(mms_timestamp, mms_author, part_ct, part_cl, part_data)
+values (?, ?, ?, ?, ?);"""
+    s.InsertMany(MIME_sql, MIME_data)
+    pass
 
 
 def load_db():
@@ -46,31 +50,42 @@ def load_db():
 
 
 def mms_parsing(message_xml):
-    date = message_xml["@date"]
+    message = {}
+    message['date'] = message_xml["@date"]
     for part in message_xml["parts"].values():
-        author = 'Rebecca'
+        message['author'] = 'Rebecca'
         if isinstance(part, list):
             for od in part:
                 seq_flag = int(od['@seq'])
                 if seq_flag == -1:
-                    author = 'Andy'
+                    message['author'] = 'Andy'
                 else:
-                    text = od["@text"]
+                    message['text'] = od["@text"]
+            message['MIME'] = False
         elif isinstance(part, dict):
             if part["@seq"] == '-1':
-                author = 'Andy'
-            text = part["@text"]
+                message['author'] = 'Andy'
+            message['text'] = part["@text"]
+            message['ct'] = part['@ct']
+            message['cl'] = part['@cl']
+            try:
+                message['data'] = part['@data']
+                message['MIME'] = True
+            except KeyError:
+                message['MIME'] = False
         else:
-            author = '<unknown>'
-            text = '<unknown>'
-    return date, author, text
+            message['author'] = '<unknown>'
+            message['text'] = '<unknown>'
+    return message
 
 
 def sms_parsing(message_xml):
-    author = 'Rebecca' # I don't actually know how to determine the author for sms message types
-    date = message_xml["@date"]
-    text = message_xml["@body"]
-    return date, author, text
+    message = {}
+    message['author'] = 'Rebecca' # I don't actually know how to determine the author for sms message types
+    message['date'] = message_xml["@date"]
+    message['text'] = message_xml["@body"]
+    message['MIME'] = False
+    return message
 
 
 def read_xml(source_file_name):
@@ -83,12 +98,17 @@ def read_xml(source_file_name):
 
 def main():
 
-    def do_append(unix_timestamp, author, text):
-        local_time = datetime.fromtimestamp(float(unix_timestamp)/1000, local_timezone)
-        all_data.append([unix_timestamp, local_time, author, text])
+    def do_append(message):
+        local_time = datetime.fromtimestamp(float(message['date'])/1000, local_timezone)
+        if message['MIME']:
+            MIME_data.append([message['date'], message['author'], message['ct'], message['cl'], message['data']])
+        else:
+            all_data.append([message['date'], local_time, message['author'], message['text']])
 
-    for source_file_name in get_file_names_from_repository():
+    # for source_file_name in get_file_names_from_repository():
+    for source_file_name in [r"H:\OneDrive\Apps\SMS Backup and Restore\done\sms-20250321031031.xml"]:
         all_data = []
+        MIME_data = []
         my_xml = read_xml(source_file_name)
         s0 = xmltodict.parse(my_xml)
         s1 = s0['smses']
@@ -98,23 +118,20 @@ def main():
             if k == 'mms':
                 counter = 0
                 for vv in v:
-                    date, author, text = mms_parsing(vv)
-                    do_append(date, author, text)
+                    message = mms_parsing(vv)
+                    do_append(message)
             elif k == 'sms':
                 for vv in v:
-                    date, author, text = sms_parsing(vv)
-                try:
-                    do_append(date, author, text)
-                except Exception as e:
-                    print(f'\n\nUnhandled exception: {e}\ndata:\n{k}\n{v}')
-                    exit()
+                    message = sms_parsing(vv)
+                do_append(message)
             elif type(v) == 'str':
                 pass
             else:
-                print(f'ignored content key {k}')
+                pass
+                # print(f'ignored content key {k}')
         print(f'{len(all_data)} records read\n')
         print('writing to database...')
-        write_to_db(all_data)
+        write_to_db(all_data, MIME_data)
         load_db()
         # remove(source_file_name)
     print('done')
